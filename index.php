7c09f0f6366680ac7b060dde2d912e3ada9f24d7
Nick <nick.deniel.em@gmail.com>
2020-11-03 17:39:36 +0200

    added main blocks for index.php

--- index.php
+++ index.php
@@ -1,3 +1,21 @@
-<?php
-echo "Hello <b>NIX Education</b>";
-?>
+<!doctype html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="css/style.css"
+    <title>Test site</title>
+</head>
+<body>
+<header class="ml"><p>header</p></header>
+<nav class="ml"><p>nav</p></nav>
+<main class="ml">
+    <p>main</p>
+    <p><a href="#">Урок 1</a></p>
+    <p><a href="#">Урок 2</a></p>
+</main>
+<footer class="ml"><p>footer</p></footer>
+</body>
+</html>
